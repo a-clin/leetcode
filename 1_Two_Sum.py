e81979f87d7363690a2eeee1c2e3c522e8aef4c1
a-clin <ablin16@hotmail.com>
2022-12-19 14:07:09 +1100

    9/12/22

---
+++ 1_Two_Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, v in enumerate(nums):
+            if (target - v) not in seen:
+                seen[v] = i
+            else:
+                return [i, seen[target-v]]
